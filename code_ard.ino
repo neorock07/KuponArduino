6c71435527e658d73b065753a1767f003da51b61
Eka Yulianto <99853004+neorock07@users.noreply.github.com>
2024-08-20 12:56:30 +0700

    Add files via upload

--- code_ard.ino
+++ code_ard.ino
@@ -1,12 +1,28 @@
 #include <ArduinoJson.h>
 #include <ArduinoJson.hpp>
-
 #include <SPI.h>
 #include <MFRC522.h>
 #include <EEPROM.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClientSecureBearSSL.h>
 #include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+
+/*
+pin esp8266 :
+SDA D2
+SCK D5
+MOSI D7
+MISO D6
+GND
+RST D1
+3V
+
+pin led :
+D8
+*/
+
 
 //konfigurasi pin untuk rfid
 #define RST_PIN D1
@@ -16,7 +32,9 @@
 #define API_MAPPING "http://192.168.43.251/ProyekKupon/api/mapping_arduino_katering/select"
 //alamat API untuk insert kupon_harian
 #define API_TRANSACTION "http://192.168.43.251/ProyekKupon/api/kupon_harian/insert"
-#define API_ARDUINO "http://192.168.43.251/ProyekKupon/api/arduino/insert"
+//alamat API untuk insert arduino
+#define API_ARDUINO "https://blbqfx2p-80.asse.devtunnels.ms/ProyekKupon/api/arduino/insert"
+#define API_RFID "https://blbqfx2p-80.asse.devtunnels.ms/ProyekKupon/api/rfid/select"
 
 //inisiasi rfid
 MFRC522 mfrc522(SS_PIN, RST_PIN);
@@ -25,6 +43,8 @@ WiFiClient wifiClient;
 //variabel untuk konek ke wifi
 const char* ssid = "Infinix HOT 8 Neo ";
 const char* password = "07072003";
+
+//inisiasi variabel audio
 
 //variabel untuk id unik alat
 String id_arduino = "AD 12 5";
@@ -39,16 +59,19 @@ int id_kantin;
 String uidCard;
 
 //led uji
-int led = 4;
+int led = D8;
 //untuk menyimpan ip_arduino
 String ip_arduino;
+//agar dapat mengirim ke https
+std::unique_ptr<BearSSL::WiFiClientSecure>clientHttps(new BearSSL::WiFiClientSecure);
+
 
 void setup() {
 
-  pinMode(led, OUTPUT);
-
   //inisiasi serial komunikasi ke 9600
   Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
   //inisiasi SPI bus
   SPI.begin();
   connectWifi();
@@ -66,14 +89,9 @@ void loop() {
   Serial.println("belum ada kartu");
   String rfid_uid = readRfid();
   if (rfid_uid != "") {
-    // if(getMapping(id_arduino)){
-    //   sendTransaksi(id_arduino, id_kantin, id_katering, 2);
-    // }
     Serial.println(rfid_uid);
-    digitalWrite(led, LOW);
   }
-  delay(3000);
-  digitalWrite(led, HIGH);
+  delay(2000);
 }
 
 /* fungsi untuk connect ke wifi */
@@ -83,14 +101,17 @@ void connectWifi() {
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(led, HIGH);
     delay(500);
     Serial.print(".");
+    digitalWrite(led, LOW);
   }
-
+  digitalWrite(led, LOW);
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+  clientHttps->setInsecure();
   ip_arduino = WiFi.localIP().toString();
 }
 
@@ -120,13 +141,28 @@ String readRfid() {
   Serial.println();
   Serial.print("UID value : ");
   Serial.println(content);
-  uidCard = content;
-  // insertArduino(content);
-  bool get_map = getMapping(id_arduino);
 
-  if (get_map == true) {
-    sendTransaksi(id_arduino, id_kantin, id_katering, content);
+  //handle jika user sudah melakukan scan
+  if (uidCard == content) {
+    Serial.println("Sudah Scan");
+    digitalWrite(led, HIGH);
+    delay(100);
+    digitalWrite(led, LOW);
+    delay(100);
+    digitalWrite(led, HIGH);
+    delay(100);
+    digitalWrite(led, LOW);
+    delay(100);
+  } else {
+    uidCard = content;
+    // insertArduino(content);
+    bool get_map = getMapping(id_arduino);
+
+    if (get_map == true) {
+      sendTransaksi(id_arduino, id_kantin, id_katering, content);
+    }
   }
+  // return hasil bacaan rfid terkini
   return content;
 }
 
@@ -143,12 +179,13 @@ bool getMapping(String id_arduino) {
 
     HTTPClient http;
     //post data id_arduino ke API (select mapping_arduino_katering)
+    // http.begin(wifiClient, API_MAPPING);
     http.begin(wifiClient, API_MAPPING);
     http.addHeader("Content-Type", "application/json");
     String jsonString;
     serializeJson(jsonDoc, jsonString);
     int responseCode = http.POST(jsonString);
-
+    Serial.println("data json : " + jsonString);
     //cek jika berhasil post data
     if (responseCode == 200) {
       String response = http.getString();
@@ -177,6 +214,54 @@ bool getMapping(String id_arduino) {
 }
 
 /*
+function untuk select data id_rfid by no_rfid : (note: for testing purpose only)
+*/
+bool getRfid(String no_rfid) {
+  //cek apakah tersambung wifi
+  if (WiFi.status() == WL_CONNECTED) {
+    StaticJsonDocument<200> jsonDoc;
+    no_rfid.trim();
+    // Masukkan string yang telah di-trim ke dalam JSON document
+    jsonDoc["no_rfid"] = no_rfid.c_str();
+    Serial.println(no_rfid);
+
+    HTTPClient http;
+    //post data id_arduino ke API (select mapping_arduino_katering)
+    http.begin(wifiClient, API_RFID);
+    http.addHeader("Content-Type", "application/json");
+    String jsonString;
+    serializeJson(jsonDoc, jsonString);
+    int responseCode = http.POST(jsonString);
+
+    //cek jika berhasil post data
+    if (responseCode == 200) {
+      String response = http.getString();
+      Serial.println("Data rfid : " + response);
+
+      //consume json response dari api
+      DynamicJsonDocument doc(1024);
+      deserializeJson(doc, response);
+
+      //apabila data diterima tidak null, maka simpan ke variabel terkait
+      if (doc["id_rfid"] != "") {
+        Serial.println("Ada rfid nih");
+      } else {
+        Serial.println("tidak ada rfid");
+      }
+      http.end();
+      return true;
+    }
+    //jika post failed maka print error
+    else {
+      Serial.println("Error get rfid : " + String(responseCode));
+      http.end();
+      return false;
+    }
+  }
+  return false;
+}
+
+/*
   function untuk post data transaksi kupon_harian
 */
 bool sendTransaksi(String id_arduino, int id_kantin, int id_katering, String no_rfid) {
@@ -185,24 +270,25 @@ bool sendTransaksi(String id_arduino, int id_kantin, int id_katering, String no_
 
     StaticJsonDocument<1024> jsonDoc;
     jsonDoc["id_arduino"] = String(id_arduino);
-    jsonDoc["id_kantin"] = 1;
-    jsonDoc["id_katering"] = 1;
-    jsonDoc["no_rfid"] = "62 D1 15 51";
+    jsonDoc["id_kantin"] = id_kantin;
+    jsonDoc["id_katering"] = id_katering;
+    no_rfid.trim();
+    // Masukkan string yang telah di-trim ke dalam JSON document
+    jsonDoc["no_rfid"] = no_rfid.c_str();
     Serial.println("Kudune ngirim sih...");
-    // Serial.println("kirim -> id arduino : " + id_arduino + " id kantin:" + String(id_kantin) + " id katering:" + String(id_katering) + " no rfid :" + no_rfid);
 
 
     HTTPClient http;
     //post data body json ke insert kupon_harian
     http.begin(wifiClient, API_TRANSACTION);
     http.addHeader("Content-Type", "application/json");
-    // String postData = "{\"id_arduino\":\"" + String(id_arduino) + "\",\"id_kantin\":\"" + String(id_kantin) + "\",\"id_katering\":\"" + String(id_katering) + "\",\"id_rfid\":\"" + String(id_rfid) + "\"}";
     String jsonString;
     serializeJson(jsonDoc, jsonString);
     int responseCode = http.POST(jsonString);
 
     //cek jika berhasil post data
     if (responseCode == 200) {
+
       String response = http.getString();
       Serial.println("Transaksi Response : " + response);
 
@@ -211,6 +297,7 @@ bool sendTransaksi(String id_arduino, int id_kantin, int id_katering, String no_
       deserializeJson(doc, response);
       String message = doc["message"].as<String>();
       String kantin = doc["kantin"].as<String>();
+      int nik = doc["nik"].as<int>();
       String status = doc["status"].as<String>();
       String error = doc["error"].as<String>();
       int jumlah = doc["jumlah"].as<int>();
@@ -218,6 +305,7 @@ bool sendTransaksi(String id_arduino, int id_kantin, int id_katering, String no_
 
       //print hasil response;
       Serial.println("Message : " + message);
+      Serial.println("nik : " + String(nik));
       Serial.println("kantin : " + kantin);
       Serial.println("status : " + status);
       Serial.println("error : " + error);
@@ -226,6 +314,9 @@ bool sendTransaksi(String id_arduino, int id_kantin, int id_katering, String no_
 
       //return true jika berhasil post
       http.end();
+      digitalWrite(led, HIGH);
+      delay(1000);
+      digitalWrite(led, LOW);
       return true;
     }
     //cek jika tidak berhasil post data
@@ -237,25 +328,4 @@ bool sendTransaksi(String id_arduino, int id_kantin, int id_katering, String no_
     }
   }
   return false;
-}
-
-//kode untuk menyimpan data ke flash memory
-// void writeMemory(String konten) {
-//   for (int i = 0; i < konten.length(); i++) {
-//     EEPROM.write(int i, konten[i]);
-//   }
-
-//   EEPROM.write(konten.length(), '\0');
-//   EEPROM.commit();
-// }
-
-//kode untuk membaca data dari flash memory
-String readMemory() {
-  String konten = "";
-  for (int i = 0; i < 20; i++) {
-    char c = EEPROM.read(i);
-    if (c == '\0') break;
-    konten += c;
-  }
-  return konten;
 }
